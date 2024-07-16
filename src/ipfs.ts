5f23e935b15d60d3a7e1140680a703fdf979f5cc
Robert Chu <allegrormc@gmail.com>
2024-07-16 16:33:21 -0700

    Update ipfs.ts

--- src/ipfs.ts
+++ src/ipfs.ts
@@ -2,7 +2,7 @@ import { CID } from "multiformats/cid";
 import * as json from "multiformats/codecs/json";
 import { sha256 } from "multiformats/hashes/sha2";
 
-export async function calculateIpfsHash(obj: any): Promise<string> {
+export const calculateIpfsHash = async (obj: any): Promise<string> => {
   const bytes = json.encode(obj);
   const hash = await sha256.digest(bytes);
   const cid = CID.create(1, json.code, hash);
